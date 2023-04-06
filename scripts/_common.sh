c83cf29009caa8ddb1a58c13232628dabaeabf1b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-06 11:04:48 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="curl"
+#REMOVEME? pkg_dependencies="curl"
 
 #=================================================
 # PERSONAL HELPERS
